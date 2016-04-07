59c0e6a8488bdf68404bed6231fb1499cd6f36e3
Hurqalia <Hurqalia@users.noreply.github.com>
2016-04-07 13:32:38 +0200

    Update drawstore.meta.js

--- drawstore.meta.js
+++ drawstore.meta.js
@@ -2,12 +2,12 @@
 // @id             iitc-plugin-drawstore
 // @name           IITC plugin: DrawStore
 // @category       Info
-// @version        0.0.1.20160407.003
+// @version        0.0.1.20160407.004
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://github.com/Hurqalia/drawstore/raw/master/drawstore.meta.js
 // @downloadURL    https://github.com/Hurqalia/drawstore/raw/master/drawstore.user.js
 // @installURL     https://github.com/Hurqalia/drawstore/raw/master/drawstore.user.js
-// @description    [hurqalia22-2016-04-07-000003] DrawStore
+// @description    [hurqalia22-2016-04-07-000004] DrawStore
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
