0477536272827b5a2ac066a9a7019aac68aca366
Hurqalia <Hurqalia@users.noreply.github.com>
2016-10-05 13:32:07 +0200

    Update drawstore.meta.js
    Fix Niantics redirect

--- drawstore.meta.js
+++ drawstore.meta.js
@@ -2,15 +2,19 @@
 // @id             iitc-plugin-drawstore
 // @name           IITC plugin: DrawStore
 // @category       Info
-// @version        0.0.1.20160407.004
+// @version        0.1.2.20160407.004
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://github.com/Hurqalia/drawstore/raw/master/drawstore.meta.js
 // @downloadURL    https://github.com/Hurqalia/drawstore/raw/master/drawstore.user.js
 // @installURL     https://github.com/Hurqalia/drawstore/raw/master/drawstore.user.js
 // @description    [hurqalia22-2016-04-07-000004] DrawStore
-// @include        https://www.ingress.com/intel*
-// @include        http://www.ingress.com/intel*
-// @match          https://www.ingress.com/intel*
-// @match          http://www.ingress.com/intel*
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
 // @grant          none
 // ==/UserScript==
